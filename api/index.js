6fd91987032a218b5c4faa94b3740bf3c79b7087
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-02-22 00:48:40 +0530

    mongonot connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config({path: '../.env' });
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("are bhai tu connect ho gya.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app= express()
 app.listen(3000,()=>{
     console.log("sunn raha hai n tu");
+    console.log(process.env.MONGO)
+  
 })
