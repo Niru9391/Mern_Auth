79e2639c92be3ad93f7cb73d0d643333f8f8cff7
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-02-22 23:25:23 +0530

    finally connected with database

--- api/index.js
+++ api/index.js
@@ -1,20 +1,24 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-dotenv.config({path: '../.env' });
+dotenv.config()
+
 
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log("are bhai tu connect ho gya.");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+.connect(process.env.MONGO)
+.then(()=>{
+  console.log("connected to database")
+})
+.catch((err)=>{
+  console.log(err);
+})
+
+
 
 const app= express()
 app.listen(3000,()=>{
     console.log("sunn raha hai n tu");
     console.log(process.env.MONGO)
+    
   
 })
