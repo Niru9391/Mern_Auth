1d65a5fc9ea206640856bedbb3e4732091557012
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-03-04 23:05:07 +0530

    create sign in api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.model.js"
 import bcryptjs from 'bcryptjs'
 import errorHandler from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const  signup = async(req,res,next)=>{
     const { username,email,password}=req.body
     
@@ -8,7 +9,7 @@ export const  signup = async(req,res,next)=>{
    
     try{
         await newUser.save()
-        res.status(201).json('successfully your data loaded to databse')
+        res.status(201).json('successfully, your data has been  loaded to databse')
     }
     catch(error){
         next(errorHandler(300,'Something is wrong please try again! '));
@@ -17,4 +18,28 @@ export const  signup = async(req,res,next)=>{
     
     
     
+}
+
+
+export const signin = async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+        const validata = await User.findOne({email});
+        if(!validata){
+            return next(errorHandler(404,'User not found'));
+
+        }
+
+       const validpassword =bcryptjs.compareSync(password,validata.password);
+       if(!validpassword){
+        return next(errorHandler(401,'Invalid information'))
+       }
+       const token = jwt.sign({id:validata._id},process.env.JWT_SECRET)
+       const {password:hashedPassword,...rest}= validata._doc;
+       res.cookie('access_token',token,{httpOnly:true,  expires:new Date()+360000}).status(200).json(validata)
+
+    }
+    catch(error){
+        next(error);
+    }
 }
