1fc14bcd6316db1a7a740fe76c6d270dc987a548
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-03-09 23:34:05 +0530

    change in  index js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ mongoose
 
 
 const app= express()
-app.use(express.static(path.join(__dirname,'/client/build')))
+app.use(express.static(path.join(__dirname,'/client/dist  ')))
 app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'/client','dist','index.html'))
 })
