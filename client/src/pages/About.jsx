4162d5c00ab55c68539ffc71540633df6ffa37ff
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-03-15 04:08:38 +0530

    complete the home and About section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      About
+      <h1 className='text-pretty text-center leading-10 font-extrabold italic'>I have completed this project in approx 15 days and I learn more about Reactjs, firebase and many more things </h1>
     </div>
   )
 }
