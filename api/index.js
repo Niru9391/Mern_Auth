ae52500375717e2bbc9e7eb7a614a6b5f9a42b8b
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-03-17 02:13:40 +0530

    change -

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const app= express()
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
